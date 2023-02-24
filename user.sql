d6519ccf6143918d843dcb1a4fe1ef6ee6fa711a
hyunjeong seo <sxxhnjng@gmail.com>
2023-02-24 18:05:49 +0900

    Update user.sql

--- user.sql
+++ user.sql
@@ -3,3 +3,16 @@ create user 'scott'@'%' identified by 'tiger';
 grant all privileges on *.* to 'scott'@localhost;
 grant all privileges on *.* to 'scott'@'%';
 flush privileges;
+
+create database lecture;
+use lecture;
+create table dept(
+deptno int,
+dname varchar(14),
+loc varchar(13)
+);
+insert into dept values (10, ‘test1’, 'test');
+insert into dept values (20, ‘test2’, 'test');
+insert into dept values (30, ‘test3’, 'test');
+insert into dept values (40, ‘test4’, 'test');
+commit;
